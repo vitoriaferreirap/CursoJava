b81b7e9f4ce4995b7455d12b6143f3b3dda7a9a2
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-03-20 17:45:25 -0300

    testando troca email

--- src/conceitos_POO/EX_11/ContaBanco.java
+++ src/conceitos_POO/EX_11/ContaBanco.java
@@ -1,65 +1,105 @@
 package conceitos_POO.EX_11;
 
+import conceitos_POO.EX_6.ContaBancaria;
+
 public class ContaBanco {
 
   public int numConta;
   protected String tipo;
-  public String titular;
-  public double saldo;
-  public boolean status;
+  private String titular;
+  private double saldo;
+  private boolean status;
+
+  public ContaBanco() {
+
+  }
 
   public ContaBanco(int numConta, String tipo, String titular) {
     this.numConta = numConta;
     this.titular = titular;
     this.tipo = tipo;
-    this.saldo = saldo;
-    this.status = status;
+    this.setSaldo(0.0);
+    this.setStatus(false);
   }
 
-  public void abrirNovaConta() {
+  public void abrirNovaConta(String tipo) {
     double mensalidade = 0.0;
-    this.status = true;
+
+    setTipo(tipo);// configurando o tipo em relacao ao que vem do teclado
+    System.out.println(getTitular());
+    setStatus(true);// config valor logico
+
     // .equals = compara string
-    if (tipo.equals("cp")) {
-      saldo += 100.00;
+    if (tipo.equals("CP")) {
+      setSaldo(100.00);
       mensalidade = 20.00;
-    } else if (tipo.equals("cc")) {
-      saldo += 50.00;
+      System.out.printf("Conta aberta com sucesso! Saldo inicial: %.2f\n", getSaldo());
+      pagarMensalidade(mensalidade);
+      // saldo += 100.00;
+    } else if (tipo.equals("CC")) {
+      setSaldo(50.00);
       mensalidade = 12.00;
+      System.out.printf("Conta aberta com sucesso! Saldo inicial: %.2f\n", getSaldo());
+      pagarMensalidade(mensalidade);
 
+      // saldo += 50.00;
     }
-    pagarMensalidade(mensalidade);
 
-  }
-
-  public void pagarMensalidade(double mensalidade) {
-    System.out.printf("Conta aberta? %b, TIPO: %s, SALDO ATUAL: %.2f\n", status, tipo, saldo);
-    System.out.printf("Sua mensalidade é de: %.2f\n", mensalidade);
-    saldo -= mensalidade;
-    System.out.printf("Saldo apos mensalidade:%.2f\n", saldo);
   }
 
   public void fecharConta() {
-    if (saldo < 0) {
+    if (getSaldo() < 0) {
       System.out.println("NÃO PODE FECHAR CONTA.Saldo negativo");
-    } else if (saldo > 0) {
+    } else if (getSaldo() > 0) {
       System.out.println("NÃO PODE FECHAR CONTA. Existe saldo na conta: " + saldo);
+    } else {
+      setStatus(false);
     }
 
   }
 
   public void depositar(double deposito) {
-    saldo += deposito;
-    System.out.printf("Saldo apos deposito: %.2f\n", getSaldo());
+    if (getStatus() == true) {
+      setSaldo(getSaldo() + deposito);// modifica o saldo para(saldoAtual + deposito) MELHOR SOLUÇÃO
+      // saldo += deposito;
+      System.out.printf("Saldo apos deposito: %.2f\n", getSaldo());
+    } else {
+      System.out.println("Impossivel depositar");
+    }
 
   }
 
   public void sacar(double saque) {
-    saldo -= saque;
-    System.out.printf("Saldo apos saque: %.2f\n", getSaldo());
+
+    if (getStatus() == true) {// conta ativa
+      if (getSaldo() >= saque) {// tem dinheiro para sacar?
+        setSaldo(getSaldo() - saque);
+        System.out.printf("Saldo apos saque: %.2f\n", getSaldo());
+      } else {
+        System.out.println("Impossivel depositar");
+      }
+    } else {
+      System.out.println("Impossivel sacar de uma conta fechada");
+    }
+  }
+
+  public void pagarMensalidade(double mensalidade) {
+
+    if (getStatus() == true) {// conta ativa
+      if (getSaldo() >= mensalidade) {// tenho valor para pagar mensalidade?
+        setSaldo(getSaldo() - mensalidade);
+        System.out.printf("Seu saldo após pagar mensalidade: %.2f\n", getSaldo());
+      } else {
+        System.out.println("Erro ao cobrar mensalidade");
+      }
+    } else {
+      System.out.println("Impossivel pagar com uma conta fechada ");
+    }
 
   }
 
+  // Argumento é o valor que você passa para a função ou método quando chama ela.
+  // Parâmetro é uma variável declarada em uma função ou método.
   public int getNumConta() {
     return numConta;
   }
--- src/conceitos_POO/EX_11/Ex_11.java
+++ src/conceitos_POO/EX_11/Ex_11.java
@@ -1,12 +1,36 @@
 package conceitos_POO.EX_11;
 
+import conceitos_POO.EX_6.ContaBancaria;
 import java.util.Scanner;
+/*REGRAS
+ * CC = conta corrente (incia c/ 50,00) CP = conta poupança (incia c/ 150,00)
+ * Ideia é mostrar criando varios objetos, e manipulado e reutilizando metodos,
+ * para termos o mesmo resultado. 
+ * Vemos duas contas sendo criadas com dados inseridos no codigo;
+ * e depois um meno que possibilita a pessoa inserir dados via teclado e os mesmos serem adicionados, assim como fazer operações de forma 
+ * que o usuario preferir
+ */
 
 public class Ex_11 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("O que deseja fazer?");
+    // dados inseridos
+    ContaBanco conta2 = new ContaBanco();
+    conta2.setNumConta(111);
+    conta2.setTitular("Vitoria");
+    conta2.abrirNovaConta("CP");
+    conta2.depositar(300.00);
+
+    ContaBanco conta3 = new ContaBanco();
+    conta3.setNumConta(222);
+    conta3.setTitular("Maria");
+    // conta3.sacar(20.00);//a conta ainda nao foi aberta
+    conta3.abrirNovaConta("CC");
+    conta3.sacar(20.00);
+
+    // conta2.statusGeral();
+    System.out.println("\nO que deseja fazer?\n");
     System.out.println("1 - Crir conta\n2 - Depositar\n3 - Sacar");
     int num = sc.nextInt();
 
@@ -22,7 +46,7 @@ public class Ex_11 {
     numConta = sc.nextInt();
 
     ContaBanco conta = new ContaBanco(numConta, tipo, titular);
-    conta.abrirNovaConta();
+    conta.abrirNovaConta(tipo);
 
     System.out.println("Qual valor deseja depositar?\n");
     double deposito = sc.nextDouble();
@@ -34,8 +58,6 @@ public class Ex_11 {
 
     System.out.println("Deseja fechar a conta?");
     conta.fecharConta();
-
     sc.close();
-
   }
 }
